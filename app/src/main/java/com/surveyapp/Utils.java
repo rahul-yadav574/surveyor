23a394038a0bf5e77be3fa5f513f5fa160a545d3
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-01-31 11:32:25 +0530

    TextWatcher Added To Login And SignUp forms

--- app/src/main/java/com/surveyapp/Utils.java
+++ app/src/main/java/com/surveyapp/Utils.java
@@ -1,6 +1,18 @@
 package com.surveyapp;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by arcks on 30/1/16.
@@ -9,5 +21,44 @@ import android.content.Context;
 
 
 public class Utils {
+
+    public static JSONObject getJSONFromUrl(String completeurl){
+        InputStream is = null;
+        JSONObject jsonObject=null;
+        String jsonstring="";
+        try {
+            URL url = new URL(completeurl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setReadTimeout(15000);
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+
+            is = new BufferedInputStream(urlConnection.getInputStream());
+            java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+            if(s.hasNext()){
+                jsonstring= s.next();
+            }
+            urlConnection.disconnect();
+        } catch (MalformedURLException e) {
+            Log.d("error", "error in getjsonfromurl MalformedUrlexception");
+        } catch (IOException e) {
+            Log.d("error", "error in getjsonfromurl Ioexception");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try {
+            jsonObject = new JSONObject(jsonstring);
+        } catch (JSONException e) {
+            Log.d("error", "Json exception in get JSONFRomURL ");
+        }
+        return jsonObject;
+    }
+
+    public static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
 
 }
