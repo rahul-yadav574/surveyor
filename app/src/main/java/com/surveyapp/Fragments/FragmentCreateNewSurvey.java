143df7ef5e0bcd1f5c3245f83009a87522815100
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-01-31 09:24:32 +0530

    Fragments And Activities Code Added

---
+++ app/src/main/java/com/surveyapp/Fragments/FragmentCreateNewSurvey.java
@@ -0,0 +1,32 @@
+package com.surveyapp.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.surveyapp.R;
+
+/**
+ * Created by abhey singh on 31-01-2016.
+ */
+public class FragmentCreateNewSurvey extends Fragment {
+
+    public FragmentCreateNewSurvey() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_create_survey,container,false);
+
+        return rootView;
+    }
+}
