794af0e46836d7e3c1825bc686b1a62376ad5235
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-02-09 23:28:10 +0530

    User Account Code Added

---
+++ app/src/main/java/com/surveyapp/Adapters/UserAccountListAdapter.java
@@ -0,0 +1,84 @@
+package com.surveyapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.surveyapp.R;
+
+import java.util.List;
+
+/**
+ * Created by Rahul Yadav on 09-02-2016.
+ */
+public class UserAccountListAdapter extends BaseAdapter {
+
+
+    private List<UserAccountObject> userCredentialList;
+    private Context context;
+
+    public UserAccountListAdapter(Context context, List<UserAccountObject> userCredentialList) {
+
+        this.context = context;
+        this.userCredentialList = userCredentialList;
+    }
+
+    @Override
+    public int getCount() {
+        return userCredentialList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return userCredentialList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View rootView = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_user_account_list,parent,false);
+
+        TextView tile = (TextView) rootView.findViewById(R.id.userCredentialsTile);
+        TextView value = (TextView) rootView.findViewById(R.id.userCredentialsValue);
+
+        tile.setText(userCredentialList.get(position).getTitle());
+        tile.setText(userCredentialList.get(position).getValue());
+
+
+        return rootView;
+    }
+
+    public class UserAccountObject {
+        private String title;
+        private String value;
+
+        public UserAccountObject(String title, String value) {
+            this.title = title;
+            this.value = value;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+}
--- app/src/main/java/com/surveyapp/Fragments/UserAccountRelated/FragmentUserAccountShow.java
+++ app/src/main/java/com/surveyapp/Fragments/UserAccountRelated/FragmentUserAccountShow.java
@@ -6,13 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.surveyapp.R;
+import com.surveyapp.SharedPrefUtil;
 
 /**
  * Created by Rahul Yadav on 06-02-2016.
  */
 public class FragmentUserAccountShow extends Fragment {
+
+    private ListView userCredentialsListView;
+    private ListView userResponseListView;
+    private Button logOutButton;
+    private SharedPrefUtil sharedPrefUtil;
 
     public FragmentUserAccountShow() {
     }
@@ -20,12 +28,26 @@ public class FragmentUserAccountShow extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.sharedPrefUtil = new SharedPrefUtil(getActivity());
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_user_account,container,false);
+
+        userCredentialsListView = (ListView)  rootView.findViewById(R.id.userCredentialsListView);
+        userResponseListView  = (ListView) rootView.findViewById(R.id.newResponsesListView);
+        logOutButton = (Button) rootView.findViewById(R.id.logOutButton);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /*Here The User Will be log Out From The App And The First Page Will Be Show*/
+            }
+        });
 
         return rootView;
     }
